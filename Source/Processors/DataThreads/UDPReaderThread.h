2a2ad3fbcc89c945a6af8dee685fbe27911479de
Marti Bolivar <mbolivar@leaflabs.com>
2013-02-13 15:04:34 -0500

    Add empty UDPReaderThread files.
    These will contain a DataThread for reading from a UDP socket.
    
    Signed-off-by: Marti Bolivar <mbolivar@leaflabs.com>

---
+++ Source/Processors/DataThreads/UDPReaderThread.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    UDPReaderThread.h
+    Created: 17 Apr 2013 12:04:10pm
+    Author:  mbolivar
+
+  ==============================================================================
+*/
+
+#ifndef __UDPREADERTHREAD_H_40D41976__
+#define __UDPREADERTHREAD_H_40D41976__
+
+
+
+
+
+#endif  // __UDPREADERTHREAD_H_40D41976__
